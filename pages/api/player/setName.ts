cd939ffdbb2f54caf501f375cd0c9318efa4c23f
jason <jason@hedmans.org>
2023-07-28 09:56:45 -0400

    isolate setName route

--- pages/api/player/setName.ts
+++ pages/api/player/setName.ts
@@ -19,35 +19,35 @@ export default async function handler(
 
     if (req.method === 'POST') {
         // get the request body json
-        const { body } = req;
-
-        if(body.playerId === undefined) {
-            res.status(400).json({message: 'Player ID is undefined'})
-            return;
-        }
-        if(body.name === undefined) {
-            res.status(400).json({message: 'Name is undefined'})
-            return;
-        }
-        if(typeof body.name !== 'string') {
-            res.status(400).json({message: 'Name is not a string'})
-            return;
-        }
-        if(body.name.length < 1) {
-            res.status(400).json({message: 'Name is empty'})
-            return;
-        }
-        if(body.name.length > 64) {
-            res.status(400).json({message: 'Name is too long'})
-            return;
-        }
-
-        const [pool, connection] = await getConnection();
-        await pool.query(upsertPlayerName(body.playerId as string, body.name as string))
-            .catch((e) => res.status(400).json({message: e.message}));
-        await closeConnection(pool, connection);
+        // const { body } = req;
+        //
+        // if(body.playerId === undefined) {
+        //     res.status(400).json({message: 'Player ID is undefined'})
+        //     return;
+        // }
+        // if(body.name === undefined) {
+        //     res.status(400).json({message: 'Name is undefined'})
+        //     return;
+        // }
+        // if(typeof body.name !== 'string') {
+        //     res.status(400).json({message: 'Name is not a string'})
+        //     return;
+        // }
+        // if(body.name.length < 1) {
+        //     res.status(400).json({message: 'Name is empty'})
+        //     return;
+        // }
+        // if(body.name.length > 64) {
+        //     res.status(400).json({message: 'Name is too long'})
+        //     return;
+        // }
+        //
+        // const [pool, connection] = await getConnection();
+        // await pool.query(upsertPlayerName(body.playerId as string, body.name as string))
+        //     .catch((e) => res.status(400).json({message: e.message}));
+        // await closeConnection(pool, connection);
         res.status(200).json({message: 'OK'});
     } else {
-        res.status(400).json({message: 'Only POST requests allowed'})
+        // res.status(400).json({message: 'Only POST requests allowed'})
     }
 }
